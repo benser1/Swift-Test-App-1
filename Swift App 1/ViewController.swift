f052a112ad0725e413e48f838ad9e95d99d87f5f
benser1 <briganenser28@gmail.com>
2016-12-03 11:43:54 -0500

    First commit

--- Swift App 1/ViewController.swift
+++ Swift App 1/ViewController.swift
@@ -10,11 +10,29 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var theLabel: UILabel!
+    
+    var tapCount = 0
+    
+    
+    @IBAction func pushButton(_ sender: Any) {
+        
+        tapCount = tapCount + 1
+        if tapCount < 10 {
+        theLabel.text = String(tapCount)
+        } else if tapCount == 10 {
+            theLabel.text = "Wow, already at 10?? 😅"
+        } else {
+            theLabel.text = String(tapCount) + " 👀"
+        }
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
     }
-
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
